8c3ae2bb331d28da5693d4080b8c7bf0725ba4fe
Siddhesh Khatavkar <siddheshkhatavkar24@gmail.com>
2026-04-25 20:29:16 -0700

    Improve modern UI and fix authentication redirects

--- Backend/settings.py
+++ Backend/settings.py
@@ -128,3 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Authentication redirects
+LOGIN_URL = '/loginn/'
+LOGIN_REDIRECT_URL = '/Admins/adminhome/'
+LOGOUT_REDIRECT_URL = '/loginn/'
